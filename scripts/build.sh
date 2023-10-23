07333d64d5434beae54c6fead2d76605649d9a25
calvin <hhaou@outlook.com>
2023-10-23 21:44:56 +0800

    script edit

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,6 @@
 
 env=$(echo $ENV)
 
-if [ "$env" = "dev" ]; then
-    echo "dev env build!"
-    docker build -t h5web .
-    docker tag h5web:latest 693541643703.dkr.ecr.cn-northwest-1.amazonaws.com.cn/h5web:latest
-fi
-
-if [ "$env" = "prod" ]; then
-    echo "prod env build!"
-    docker build -t tutu-backend-bff-prod .
-    docker tag tutu-backend-bff-prod:latest 693541643703.dkr.ecr.cn-northwest-1.amazonaws.com.cn/tutu-backend-bff-prod:latest
-fi
+  echo "dev env build!"
+  docker build -t h5web .
+  docker tag h5web:latest 693541643703.dkr.ecr.cn-northwest-1.amazonaws.com.cn/h5web:latest
